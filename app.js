8b73c01cac06139c01464b900fdfcd133c0ceec7
yaroslavr <rostanets@gmail.com>
2022-04-26 00:29:51 +0300

    upd

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static('dist'));
+app.get('/test', function (req, res) {
+  res.send('Hello World')
+})
+
+app.listen(3000)
