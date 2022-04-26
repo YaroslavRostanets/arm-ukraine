d58861145d696b47eab18880907d1ef44d4b9f4b
yaroslavr <rostanets@gmail.com>
2022-04-26 22:02:09 +0300

    upd

--- src/components/Body.vue
+++ src/components/Body.vue
@@ -4,33 +4,55 @@
       <div class="zsu-logo">
         <img src="/img/zsu_logo.svg" alt="logo">
       </div>
-      <button class="arm-button">
+      <button @click="showModal" class="arm-button">
         support financially
       </button>
     </div>
     <div class="text">
-      On March 24, Russia launched a large-scale invasion of Ukraine along the entire valley of the cordon and from the territory of Belarus.
-      Ukraine to repair the baked opera. It’s a pity to wage war at an extremely high cost. In Russia, there are more resources for waging war.
-      We want to live in our own country, so we have the strength to defend ourselves.
-      On the 24th of February, the National Bank opened a special fundraiser for the collection of coins to support the Defense Forces of Ukraine.
-      To my dermal one, who knows the ability to support the Ukrainian Army. Appreciate your support, it’s more important at this hour.
-      I seem to be at the Zbroyna Forces of Ukraine at once.
-      If you do not trust this website, make a donation on
-      <a href="https://bank.gov.ua/ua/news/all/natsionalniy-bank-vidkriv-rahunok-dlya-gumanitarnoyi-dopomogi-ukrayintsyam-postrajdalim-vid-rosiyskoyi-agresiyi">
-        the official website of the National Bank of Ukraine
-      </a>
-      So repost is in social networks.
+      On February 24, Russia launched a large-scale invasion of Ukraine along the entire valley of the cordon and from the territory of Belarus.
+      The Armed Forces of Ukraine are fighting bravely.
+      But waging a full-scale war with the world's largest country is very expensive.
+      We want to live in our own country, therefore we are forced to defend ourselves.
+      Russian troops rape women and children, kill civilians and commit other war crimes.
+      The National Bank of Ukraine opened a special account to raise funds to support the armed forces of Ukraine.
+      Every euro or dollar you donate can save someone's life and bring our victory closer.
+      We appreciate your support.
+      <p>
+        If you do not trust this site, make a donation
+        <a href="https://bank.gov.ua/ua/news/all/natsionalniy-bank-vidkriv-rahunok-dlya-gumanitarnoyi-dopomogi-ukrayintsyam-postrajdalim-vid-rosiyskoyi-agresiyi">
+          the official website of the National Bank of Ukraine
+        </a>.
+      </p>
+      <p>
+        We will be grateful for the distribution in social networks
+      </p>
       <SocialShare />
     </div>
+    <PayModal :show-modal="isShowModal" @closed="closeModal" />
   </div>
 </template>
 
 <script>
 import SocialShare from "@/components/SocialShare";
+import PayModal from "@/components/PayModal";
 export default {
   name: "BodyPart",
   components: {
-    SocialShare
+    SocialShare,
+    PayModal
+  },
+  data() {
+    return {
+      isShowModal: false
+    }
+  },
+  methods: {
+    showModal() {
+      this.isShowModal = true;
+    },
+    closeModal() {
+      this.isShowModal = false;
+    }
   }
 }
 </script>
---
+++ src/components/PayModal.vue
@@ -0,0 +1,64 @@
+<template>
+  <vue-final-modal v-model="isShow" @closed="$emit('closed')" classes="modal-container" content-class="modal-content">
+    <iframe frameborder="0" allowtransparency="true" allowpaymentrequest="true" allow="payment" scrolling="no"
+            style="width: 100%; height: 620px; background-color: transparent;"
+            src="https://www.liqpay.ua/en/checkoutjs?hostname=bank.gov.ua&amp;port=&amp;protocol=https%3A&amp;uid=1650997941725&amp;data=eyJ2ZXJzaW9uIjozLCJhY3Rpb24iOiJwYXlkb25hdGUiLCJhbW91bnQiOiIwIiwiY3VycmVuY3kiOiJVQUgiLCJkZXNjcmlwdGlvbiI6ItCX0LHRltGAINC60L7RiNGC0ZbQsiDQvdCwINC%2F0L7RgtGA0LXQsdC4INCw0YDQvNGW0Zcg0KPQutGA0LDRl9C90LgiLCJwdWJsaWNfa2V5IjoiaTE3NDcyMDU5OTY0IiwibGFuZ3VhZ2UiOiJ1ayJ9&amp;signature=XxmpvLEi%2FWtuWyzvF3thmiPDLyY%3D&amp;embedTo=%23liqpay_checkout&amp;language=uk&amp;mode=embed"></iframe>
+  </vue-final-modal>
+</template>
+
+<script>
+import {VueFinalModal} from 'vue-final-modal'
+
+export default {
+  name: "PayModal",
+  props: {
+    showModal: {
+      type: Boolean,
+      required: true
+    }
+  },
+  data() {
+    return {
+      isShow: this.showModal
+    }
+  },
+  watch: {
+    showModal(val) {
+      if (val) {
+        this.isShow = val;
+      }
+    }
+  },
+  components: {
+    VueFinalModal
+  }
+}
+</script>
+
+<style scoped>
+.dark-mode div::v-deep .modal-content {
+  border-color: #2d3748;
+  background-color: #1a202c;
+}
+
+::v-deep .modal-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+::v-deep .modal-content {
+  display: flex;
+  flex-direction: column;
+  margin: 0 1rem;
+  padding: 1rem;
+  border: 1px solid #e2e8f0;
+  border-radius: 0.25rem;
+  background: #fff;
+}
+
+.modal__title {
+  font-size: 1.5rem;
+  font-weight: 700;
+}
+</style>
